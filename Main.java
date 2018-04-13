9a61dcc4a121ee09bb0770f45e23bb117c044f42
Студент <student@fit.rshu.ru>
2018-04-13 14:42:12 +0300

    Initial commit

---
+++ Main.java
@@ -0,0 +1,9 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+	String[] BOBrishev = {1.1, 2.2, 3.3, 4.4 };
+	System.out.println(BOBrishev[0]);
+    }
+}
